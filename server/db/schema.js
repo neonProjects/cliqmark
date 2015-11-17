6f06c82983ac68a73a6a409b9148cdd845917e8f
rascle <ryan.bascle@gmail.com>
2015-11-17 16:31:52 -0600

    Untested: Added helpers - createUser, authUser, createBookmark, getBookmarks, removeBookmarks

--- server/db/schema.js
+++ server/db/schema.js
@@ -2,11 +2,17 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize('cliqmark', 'username', 'password', {
   host: process.ENV.port
 });
+var Promise = require('bluebird');
+var bcrypt = require('bcrypt-nodejs');
 
 //create a Users table
 var User = sequelize.define('users', {
   userId: Sequelize.UUID,
-  username: Sequelize.STRING,
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
   password: Sequelize.STRING
 });
 
@@ -14,19 +20,32 @@ var User = sequelize.define('users', {
 var Bookmark = sequelize.define('bookmarks', {
   bookmarkId: Sequelize.UUID,
   title: Sequelize.STRING,
-  url: Sequelize.STRING,
-  baseUrl: Sequelize.STRING,
+  url: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  baseUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   snapshotUrl: Sequelize.STRING, //URL from server
   text: Sequelize.Text
 }); 
 
 //create Tags table
 var Tag = sequelize.define('tags', {
-  tagId = Sequelize.UUID,
-  tagName = Sequelize.STRING
+  tagId: Sequelize.UUID,
+  tagName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  }
 });
 
-//create join tables
+//create the tables if they don't exist
+sequelize.sync();
+
+//CREATE RELATIONS
 //Bookmarks have one user, users have many bookmarks
 Bookmark.hasOne(User);
 //Bookmarks have many tags, and tags have many bookmarks
@@ -38,10 +57,110 @@ Tag.belongsToMany(Bookmark, { through: "BookmarkTags" });
 //HELPER FUNCTIONS FOR QUERYING DATABASE//
 //////////////////////////////////////////
 
-createUser(username, password, callback); // callback has (err, userId) arguments
-authUser(username, password, callback); // callback has (err, userId) arguments
-createBookmark(userId, title, url, snapshotUrl, baseUrl, text, callback); //callback has (err, bookmarkId) arguments
-getBookmarks(userId, callback); // callback has (err, bookmarks) arguments. bookmarks is array bookmark objects
-removeBookmark(bookmarkId, callback); // callback has (err) argument, only passed when error occurs
-addTag(tagName, bookmarkId, callback); //  callback has (err, tagId) arguments
-removeTag(tagName, bookmarkId, callback); // callback has (err) arguments
+//check if user exists, and create a new user with a hashed password
+var createUser = function(user, pw, callback) {
+  User.findOne({ where: {
+    username: user,
+  }).success(function(user) {
+    if (user) {
+      callback(function(err) {
+        console.log("Username already taken")
+      });
+    } else {
+      return bcrypt.hash(pw, null, null).then(function(hash) {
+        User.create({ username: user, password: hash }).success(function(user) {
+          if (user) {
+            callback(null, user.userId);
+          } else {
+            callback(function() {
+              console.log("Unable to create user.")
+            });
+          }
+        });
+      });
+    }
+  });
+
+};
+
+//Check if user exists, check if password is correct, and pass a boolean to the callback
+var authUser = function(user, password, callback) {
+  User.findOne({ where: { username: user } }).success(function(user) {
+    if (!user) {
+      callback(function() {
+        console.log("Incorrect username or password.")
+      })
+    } else {
+      bcrypt.compare(password, user.password, function(err, passwordMatch) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, passwordMatch);
+        }
+      });
+    }
+  });
+};
+
+//check if bookmark exists, and create a new bookmark
+var createBookmark = function(userId, title, url, snapshotUrl, baseUrl, text, callback) {
+  Bookmark.findOrCreate({
+    userId: userId,
+    title: title,
+    url: url,
+    snapshotUrl: snapshotUrl,
+    baseUrl: baseUrl,
+    text: text
+  }).success(function(bookmark, created) {
+    if (created) {
+      callback(null, bookmark.bookmarkId);
+    } else {
+      callback(function() {
+        console.log("You've already bookmarked this page.");
+      });
+    }
+  });
+};
+
+//get all bookmarks with a the logged in users ID
+var getBookmarks = function(userId, callback) {
+  Bookmark.findAll({ where: { userId: userId } }).then(function(bookmarks) {
+    if (bookmarks.length) {
+      callback(null, bookmarks);
+    } else {
+      callback(function() {
+        console.log("We didn't find any bookmarks");
+      });
+    }
+  })
+};
+
+//remove a bookmark with the given bookmark ID
+var removeBookmark = function(bookmarkId, callback) {
+  Bookmark.findOne({ where: { bookmarkId: bookmarkId } }).then(function(bookmark) {
+    bookmark.destroy().then(function() {
+      if (bookmark) {
+        callback(function() {
+          console.log("Could not delete bookmark");
+        })
+      }
+    })
+  })
+};
+
+// callback has (err) argument, only passed when error occurs
+var addTag = function(tagName, bookmarkId, callback) {
+  Bookmark.addTag('tag', { tagname: tagName})
+};
+
+//  callback has (err, tagId) arguments
+var removeTag = function(tagName, bookmarkId, callback) {
+
+};
+
+// callback has (err) arguments
+
+var hash = function(password) {
+  //run the password through bcrypt
+  //return the hashed password
+}
