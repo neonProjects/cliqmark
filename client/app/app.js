ef024c628289f25441d2de5d108cb968872abc2f
rascle <ryan.bascle@gmail.com>
2015-11-19 17:34:47 -0600

    Fixed Angular routing and authing. Added extension route and contoller.

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,5 @@
 var app = angular.module('cliqmark', [
+    'cliqmark.extension'
     'cliqmark.bookmarks',
     'cliqmark.auth',
     'cliqmark.services',
@@ -7,9 +8,14 @@ var app = angular.module('cliqmark', [
 
 app.config(function($routeProvider, $httpProvider) {
   $routeProvider
+    .when('/extension', {
+      templateUrl: 'app/extension/extension.html',
+      controller: 'ExtensionsController',
+    })
     .when('/bookmarks', {
       templateUrl: 'app/bookmarks/bookmarks.html',
-      controller: 'BookmarksController'
+      controller: 'BookmarksController',
+      authenticate: true
     })
     .when('/login', {
       templateUrl: 'app/auth/login.html',
@@ -22,6 +28,22 @@ app.config(function($routeProvider, $httpProvider) {
     .otherwise({
       redirectTo: '/bookmarks'
     });
+})
+
+.run(function ($rootScope, $location, Auth) {
+  // here inside the run phase of angular, our services and controllers
+  // have just been registered and our app is ready
+  // however, we want to make sure the user is authorized
+  // we listen for when angular is trying to change routes
+  // when it does change routes, we then look for the token in localstorage
+  // and send that token to the server to see if it is a real user or hasn't expired
+  // if it's not valid, we then redirect back to signin/signup
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      console.log("Routing and authing!")
+      $location.path('/login');
+    }
+  });
 });
 
 //todo: put this in another file
--- client/app/bookmarks/bookmarks.js
+++ client/app/bookmarks/bookmarks.js
@@ -1,7 +1,7 @@
 //todo move the bookmark controller in here
 angular.module('cliqmark.bookmarks', [])
 
-.controller('BookmarksController', function($scope, Bookmarks) {
+.controller('BookmarksController', function($scope, $location, $window, Bookmarks) {
   $scope.data = {};
   $scope.getBookmarks = function() {
     //todo: replace this fake data with real call to DB
@@ -15,5 +15,12 @@ angular.module('cliqmark.bookmarks', [])
         console.log(err);
       });
   };
+
+  $scope.logout = function() {
+    $window.localStorage.removeItem('cliqmark_user');
+    $window.localStorage.removeItem('cliqmark_token');
+    $location.path('/login');
+  }
+
   $scope.getBookmarks();
 });
