d546a92b4961fcbb9efc9f0397670d360d06c03b
ahrobinson <ahrobins3@gmail.com>
2015-12-01 22:26:52 -0600

    got rid of browser tab, added right click menu

--- cliqmark-extension/bookmark.js
+++
@@ -1,5 +0,0 @@
-// This is the context script that runs in the browser window (Not the extension) and has 
-// access to user session info on window.localStorage
-chrome.runtime.sendMessage({userId: window.localStorage.cliqmark_user}, function(response) {
-  console.log('response received');
-});
