08bd95c48a4f8beac031323709cb221777bf39d6
ahrobinson <ahrobins3@gmail.com>
2015-12-02 18:15:58 -0600

    add alchemy api

--- server/utility.js
+++ server/utility.js
@@ -4,6 +4,9 @@ var md5 = require('md5');
 var urlToImage = require('url-to-image');
 var easyimg = require('easyimage');
 var sanitizeHtml = require('sanitize-html');
+//Create the AlchemyAPI object
+var AlchemyAPI = require('./alchemyapi');
+var alchemyapi = new AlchemyAPI();
 
 
 var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
@@ -132,3 +135,7 @@ exports.createSession = function(req, res, newUser) {
       // res.redirect('/');
     });
 };
+
+exports.taxonomy = function(req,res){
+  
+}
